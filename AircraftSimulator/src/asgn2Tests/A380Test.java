0fb8680e1647a6bd3050fd18ce2e364f26627d55
death667b <iw.daniel@student.qut.edu.au>
2016-05-18 18:53:58 +1000

    do we now work?

---
+++ AircraftSimulator/src/asgn2Tests/A380Test.java
@@ -0,0 +1,211 @@
+/**
+ * 
+ */
+package asgn2Tests;
+
+import static org.junit.Assert.*;
+
+import org.junit.Before;
+import org.junit.Test;
+
+import asgn2Aircraft.A380;
+import asgn2Aircraft.Aircraft;
+
+/**
+ * @author user
+ *
+ */
+public class A380Test {
+
+	/**
+	 * @throws java.lang.Exception
+	 */
+	@Before
+	public void setUp() throws Exception {
+	}
+
+	/**
+	 * Test method for {@link asgn2Aircraft.A380#A380(java.lang.String, int)}.
+	 */
+	@Test
+	public void testA380StringInt() {
+		fail("Not yet implemented"); // TODO
+	}
+	
+    @Test
+    public void testAircraft() throws AircraftException {
+        aircraft = new A380("string", 123);
+
+        assertNotNull(aircraft);
+    }
+	
+
+	/**
+	 * Test method for {@link asgn2Aircraft.A380#A380(java.lang.String, int, int, int, int, int)}.
+	 */
+	@Test
+	public void testA380StringIntIntIntIntInt() {
+		fail("Not yet implemented"); // TODO
+	}
+
+	/**
+	 * Test method for {@link asgn2Aircraft.Aircraft#Aircraft(java.lang.String, int, int, int, int, int)}.
+	 */
+	@Test
+	public void testAircraft2() {
+		fail("Not yet implemented"); // TODO
+	}
+
+	/**
+	 * Test method for {@link asgn2Aircraft.Aircraft#cancelBooking(asgn2Passengers.Passenger, int)}.
+	 */
+	@Test
+	public void testCancelBooking() {
+		fail("Not yet implemented"); // TODO
+	}
+
+	/**
+	 * Test method for {@link asgn2Aircraft.Aircraft#confirmBooking(asgn2Passengers.Passenger, int)}.
+	 */
+	@Test
+	public void testConfirmBooking() {
+		fail("Not yet implemented"); // TODO
+	}
+
+	/**
+	 * Test method for {@link asgn2Aircraft.Aircraft#finalState()}.
+	 */
+	@Test
+	public void testFinalState() {
+		fail("Not yet implemented"); // TODO
+	}
+
+	/**
+	 * Test method for {@link asgn2Aircraft.Aircraft#flightEmpty()}.
+	 */
+	@Test
+	public void testFlightEmpty() {
+		fail("Not yet implemented"); // TODO
+	}
+
+	/**
+	 * Test method for {@link asgn2Aircraft.Aircraft#flightFull()}.
+	 */
+	@Test
+	public void testFlightFull() {
+		fail("Not yet implemented"); // TODO
+	}
+
+	/**
+	 * Test method for {@link asgn2Aircraft.Aircraft#flyPassengers(int)}.
+	 */
+	@Test
+	public void testFlyPassengers() {
+		fail("Not yet implemented"); // TODO
+	}
+
+	/**
+	 * Test method for {@link asgn2Aircraft.Aircraft#getBookings()}.
+	 */
+	@Test
+	public void testGetBookings() {
+		fail("Not yet implemented"); // TODO
+	}
+
+	/**
+	 * Test method for {@link asgn2Aircraft.Aircraft#getNumBusiness()}.
+	 */
+	@Test
+	public void testGetNumBusiness() {
+		fail("Not yet implemented"); // TODO
+	}
+
+	/**
+	 * Test method for {@link asgn2Aircraft.Aircraft#getNumEconomy()}.
+	 */
+	@Test
+	public void testGetNumEconomy() {
+		fail("Not yet implemented"); // TODO
+	}
+
+	/**
+	 * Test method for {@link asgn2Aircraft.Aircraft#getNumFirst()}.
+	 */
+	@Test
+	public void testGetNumFirst() {
+		fail("Not yet implemented"); // TODO
+	}
+
+	/**
+	 * Test method for {@link asgn2Aircraft.Aircraft#getNumPassengers()}.
+	 */
+	@Test
+	public void testGetNumPassengers() {
+		fail("Not yet implemented"); // TODO
+	}
+
+	/**
+	 * Test method for {@link asgn2Aircraft.Aircraft#getNumPremium()}.
+	 */
+	@Test
+	public void testGetNumPremium() {
+		fail("Not yet implemented"); // TODO
+	}
+
+	/**
+	 * Test method for {@link asgn2Aircraft.Aircraft#getPassengers()}.
+	 */
+	@Test
+	public void testGetPassengers() {
+		fail("Not yet implemented"); // TODO
+	}
+
+	/**
+	 * Test method for {@link asgn2Aircraft.Aircraft#getStatus(int)}.
+	 */
+	@Test
+	public void testGetStatus() {
+		fail("Not yet implemented"); // TODO
+	}
+
+	/**
+	 * Test method for {@link asgn2Aircraft.Aircraft#hasPassenger(asgn2Passengers.Passenger)}.
+	 */
+	@Test
+	public void testHasPassenger() {
+		fail("Not yet implemented"); // TODO
+	}
+
+	/**
+	 * Test method for {@link asgn2Aircraft.Aircraft#initialState()}.
+	 */
+	@Test
+	public void testInitialState() {
+		fail("Not yet implemented"); // TODO
+	}
+
+	/**
+	 * Test method for {@link asgn2Aircraft.Aircraft#seatsAvailable(asgn2Passengers.Passenger)}.
+	 */
+	@Test
+	public void testSeatsAvailable() {
+		fail("Not yet implemented"); // TODO
+	}
+
+	/**
+	 * Test method for {@link asgn2Aircraft.Aircraft#toString()}.
+	 */
+	@Test
+	public void testToString() {
+		fail("Not yet implemented"); // TODO
+	}
+
+	/**
+	 * Test method for {@link asgn2Aircraft.Aircraft#upgradeBookings()}.
+	 */
+	@Test
+	public void testUpgradeBookings() {
+		fail("Not yet implemented"); // TODO
+	}
+
+}
