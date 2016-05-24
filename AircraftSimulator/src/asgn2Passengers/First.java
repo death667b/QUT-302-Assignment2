2e09a7954b1208ebb760f6800928452183a8044e
death667b <iw.daniel@student.qut.edu.au>
2016-05-24 17:02:09 +1000

    Completed the constructor for the subclasses - Business, First etc

--- AircraftSimulator/src/asgn2Passengers/First.java
+++ AircraftSimulator/src/asgn2Passengers/First.java
@@ -20,7 +20,7 @@ public class First extends Passenger {
 	 * @see asgnPassengers.Passenger#Passenger(int,int)
 	 */
 	public First(int bookingTime, int departureTime) throws PassengerException {
-		//Call here 
+		super(bookingTime, departureTime);
 		this.passID = "F:" + this.passID;
 	}
 	
