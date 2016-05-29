d13cbfb24e401d3ba2ab968f019b8a993bd64fa4
Ian Daniel <iw.daniel@student.qut.edu.au>
2016-05-29 12:33:07 +1000

    Perrrrrrfect.....

--- AircraftSimulator/src/asgn2Simulators/Constants.java
+++ AircraftSimulator/src/asgn2Simulators/Constants.java
@@ -25,12 +25,12 @@ public class Constants {
 	//RNG and Probs - assume mean daily bookings around total capacity 
 	public static final int DEFAULT_SEED = 100; 
 	public static final int DEFAULT_MAX_QUEUE_SIZE = 500;
-	public static final double DEFAULT_DAILY_BOOKING_MEAN = 1300.0;
+	public static final double DEFAULT_DAILY_BOOKING_MEAN = 1800.0;
 	public static final double DEFAULT_DAILY_BOOKING_SD = 0.33*Constants.DEFAULT_DAILY_BOOKING_MEAN;
 	public static final int MINIMUM_BOOKINGS = 300;
 	public static final double DEFAULT_FIRST_PROB = 0.03;
 	public static final double DEFAULT_BUSINESS_PROB = 0.14;
 	public static final double DEFAULT_PREMIUM_PROB = 0.13;
-	public static final double DEFAULT_ECONOMY_PROB = 0.70;
+	public static final double DEFAULT_ECONOMY_PROB = 0.7;
 	public static final double DEFAULT_CANCELLATION_PROB = 0.1;
 }
